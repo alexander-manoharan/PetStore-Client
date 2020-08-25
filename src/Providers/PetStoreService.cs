bd266d4bbc5d68aef7f9647c41e39f6bc07a746c
Alexander Manoharan <alexander.manoharan@unifysolutions.net>
2020-08-25 13:04:37 +1000

    petstoreservice default implementation

--- src/Providers/PetStoreService.cs
+++ src/Providers/PetStoreService.cs
@@ -1,9 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PetStore.Demo.Models;
 using PetStore.Demo.Serices;
 
 namespace PetStore.Demo.Providers
 {
     public class PetStoreService : IPetStoreService
     {
-        
+        Task<IEnumerable<Pet>> IPetStoreService.GetPetsByCategoryAsync()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
